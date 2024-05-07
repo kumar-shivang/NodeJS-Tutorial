c1967349137009f7818fb1a117f6af202aefb64c
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-07 19:15:15 +0530

    added views to the server

--- tutorial 2/server.js
+++ tutorial 2/server.js
@@ -2,6 +2,30 @@ const http = require("http");
 
 let server = http.createServer((req, res) => {
   console.log("request made");
+  console.log(req.url, req.method);
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+  const fs = require("fs");
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
