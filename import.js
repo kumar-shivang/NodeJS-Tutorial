8dff8cb735e2aca3a40a24e472c116df6c0373a4
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-07 18:01:39 +0530

    added a bunch of files

---
+++ import.js
@@ -0,0 +1,8 @@
+const arrays = require("./modules.cjs");
+
+console.log(arrays.arr);
+console.log(arrays.arr2);
+
+const { arr } = require("./modules.cjs");
+
+console.log(arr);
---
+++ os.js
@@ -0,0 +1,6 @@
+const os = require("os");
+
+console.log(os.platform());
+console.log(os.type());
+console.log(os.machine());
+console.log(os.homedir());
