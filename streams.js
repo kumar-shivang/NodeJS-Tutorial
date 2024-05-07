187229c26e04ae99f24c63bc0316d4a118a5ec6c
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-07 18:12:24 +0530

    added more details

--- streams.js
+++ streams.js
@@ -5,8 +5,12 @@ const fs = require("fs");
 let readstream = fs.createReadStream("./lorem.txt");
 let writestream = fs.createWriteStream("./loremcopy.txt");
 
-readstream.on("data", (chunk) => {
-  console.log("new chunk recieved\n\n\n\n\n");
-  console.log(chunk.toString());
-  writestream.write(chunk);
-});
+// readstream.on("data", (chunk) => {
+//   console.log("new chunk recieved\n\n\n\n\n");
+//   console.log(chunk.toString());
+//   writestream.write(chunk);
+// });
+
+// alternatively
+
+readstream.pipe(writestream);
