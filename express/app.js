878b939fb032f391fd60911c501381dbeb143b29
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-08 23:28:20 +0530

    feat: Connect app to MongoDB database
    This commit connects the application to a MongoDB database using the provided URI. It establishes a connection to the database, listens on port 3000, and logs a message when the connection is successful.

--- express/app.js
+++ express/app.js
@@ -1,12 +1,42 @@
 // import { express } from "express";
 const express = require("express");
 const morgan = require("morgan");
+const Blog = require("../models/blog");
 const app = express();
+const dbURI = "mongodb+srv://shivangkumar1857:PE6Pk2LbHL.BEQ@blog-db.kmhjijg.mongodb.net/?retryWrites=true&w=majority&appName=blog-db"
+const mongoose = require("mongoose");
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("database conntected");
+    app.listen(3000);
+    console.log("listening on port 3000");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000);
 app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
+app.use(express.static("public"));
+
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/", (req, res) => {
   let blogs = [
     {
